33235c3ead0daf2f44b8845d38808f7a9dc8dbeb
WERKOI <korostin04@list.ru>
2022-04-30 15:03:03 +0300

    add

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
-<script>
-	export let name;
+<script>//отвечает за создание функционала страницы
+	import Table from "./Table.svelte";// импорт файлов в App.svelte
+	import Form from "./Form.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
+<style>/*отвечает за стиль страницы*/
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		flex: 1;
+		align-content: center;
+		justify-content: center;
+		width: 80%;
 		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+
+<main> <!--основное содержимое App.svelte-->
+	<h1>Project Estimator</h1>
+
+	<Form /> <!--Вызов Form.svelte-->
+
+	<Table />
+</main>
---
+++ src/Form.svelte
@@ -0,0 +1,12 @@
+<form><!--создаём форму-->
+   <fieldset><!--используется для группировки нескольких элементов управления-->
+        <label for="">Material</label><!--представляет заголовок для элемента-->
+        <input 
+        placeholder="Wood, Glue, Etc"
+        type="text" 
+        id nameField><!--приём данных-->
+   </fieldset> 
+
+
+
+</form>
