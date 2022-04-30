ec1314ac8ab672b7e7df422a37c222a696c90da9
WERKOI <korostin04@list.ru>
2022-04-30 15:50:57 +0300

    add

--- src/Form.svelte
+++ src/Form.svelte
@@ -1,12 +1,70 @@
-<form><!--создаём форму-->
+<script>
+   export let id;   
+   export let name="";
+   export let price;
+
+   $: mode = id ? "edit" : "add";
+   $: console.log(price);
+   $: canSubmit = price >= 0 && name !== "";
+
+   function submit() {
+      if (!canSubmit) {
+         return;
+      }
+
+      price = "";
+      name = "";
+      id = undefined;
+   }
+
+   function cansel(){
+      price = "";
+      name = "";
+      id = undefined;     
+   }
+</script>
+
+<style>/*отвечает за стиль кнопок*/
+   button {
+      margin-left: 20px;
+   }
+   button:disabled{
+      cursor: not-allowed;
+   }
+</style>
+
+<form on:submit|preventDefault={submit}><!--создаём форму-->
    <fieldset><!--используется для группировки нескольких элементов управления-->
-        <label for="">Material</label><!--представляет заголовок для элемента-->
+        <label for="nameField">Material</label><!--представляет заголовок для элемента-->
         <input 
+        bind:value={name} 
         placeholder="Wood, Glue, Etc"
         type="text" 
-        id nameField><!--приём данных-->
+        id="nameField"><!--приём данных-->
+
+        <label for="priceField">Price</label><!--представляет заголовок для элемента-->
+        <input
+        bind:value={price}
+        min="0"
+        step="any" 
+        placeholder="Price"
+        type="number" 
+        id="priceField">
    </fieldset> 
+   
+   <button 
+   disabled={!canSubmit}
+   class="float-right"
+   type="submit">
+   {mode}
+   </button><!--кнопка добавить-->
 
-
-
+   {#if mode === 'edit'}
+      <button 
+      on:click={cansel} 
+      class="float-right" 
+      type="button">
+      Cancel
+      </button><!--кнопка отменить-->
+   {/if}
 </form>
